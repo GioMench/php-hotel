b51a2457ba86369b16f14f71e7883f5e95a48c37
GioMench <giorgiaglm@gmail.com>
2024-04-15 15:37:00 +0200

    style table of hotel

--- index.php
+++ index.php
@@ -49,29 +49,59 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HOTEL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-
-    <?php foreach ($hotels as $hotel): ?>
-
-
-        <p>
-            <?= $hotel["name"] ?>
-            <?= $hotel["description"] ?>
-            <?= $hotel["distance_to_center"] ?>
-            <?= $hotel["vote"] ?>
-            <?= $hotel["parking"] ?>
-        </p>
-
-
-    <?php endforeach; ?>
-
-
+    <header>
+        <h1 class="text-center text-uppercase fw-bold text-light">hotel</h1>
+    </header>
+    <main>
+        <div class="container my-5">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">HOTEL</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Distance to center</th>
 
 
+                    </tr>
+                </thead>
+                <?php foreach ($hotels as $hotel): ?>
+
+                    <tbody>
+                        <tr>
+                            <th scope="row"><i class="fa-solid fa-hotel"></i></th>
+                            <td><?= $hotel["name"] ?></td>
+                            <td><?= $hotel["description"] ?></td>
+                            <td><?= $hotel["vote"] ?></td>
+                            <td><?= $hotel["parking"] ?></td>
+                            <td><?= $hotel["distance_to_center"] ?></td>
+                        </tr>
+
+                    </tbody>
+
+                <?php endforeach; ?>
+
+            </table>
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
+
 
 </html>
